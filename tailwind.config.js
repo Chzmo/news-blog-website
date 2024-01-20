a992a011da1eb2d513504b73419a3fa5753ca3e4
Chzmo <chisomomoyo10@gmail.com>
2024-01-20 10:28:07 +0200

    configured with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
-export const theme = {
-	extend: {},
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
-export const plugins = [];
