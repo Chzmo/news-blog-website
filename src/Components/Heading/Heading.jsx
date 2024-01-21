3476d29f071322b852954de5fede024c085e0337
Chzmo <chisomomoyo10@gmail.com>
2024-01-21 11:47:49 +0200

    news sections

--- src/Components/Heading/Heading.jsx
+++ src/Components/Heading/Heading.jsx
@@ -1,7 +1,7 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-/* eslint-disable react/prop-types */
 const data = {
 	title: "DON'T MISS",
 	categories: [
@@ -14,21 +14,21 @@ const data = {
 	],
 };
 function Heading() {
-	const color = "#f9c100";
-	const [bg_color, setBg_color] = useState("bg-[" + "#f9c100" + "]");
+	const hover_color = "hover:text-[#f9c100]";
+	const border = "border-b-[2px] border-b-[#f9c100]";
+	const bg_color = "bg-[#f9c100]";
+	const text_color="text-[#f9c100]";
 	return (
-		<div
-			className={`w-full flex justify-between border-b-[2px] border-b-[${color}]`}>
+		<div className={`w-full flex justify-between ${border}`}>
 			<div className={"flex item-center p-[3px] text-[14px] " + bg_color}>
 				{data.title}
-				{bg_color}
 			</div>
 			<div className='flex item-center gap-3'>
 				{data.categories.map((category, index) => (
 					<Link
 						key={index}
-						className={`text-[#3d3d3d] text-sm hover:text-[${color}] font-medium ${
-							index == 0 && "text-[${text_color}]"
+						className={`text-[#3d3d3d] text-sm font-medium ${hover_color} ${
+							index == 0 && text_color
 						}`}>
 						{category}
 					</Link>
