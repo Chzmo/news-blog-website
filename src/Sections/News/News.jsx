ec0c37dff7fe3f38a535d1b5e6f2c7a03d6ab3db
Chzmo <chisomomoyo10@gmail.com>
2024-01-29 15:03:10 +0200

    added LATEST ARTICLES

--- src/Sections/News/News.jsx
+++ src/Sections/News/News.jsx
@@ -16,6 +16,25 @@ import image_max from "../../assets/images/maxresdefault-7-485x360.jpg.webp";
 import image_54 from "../../assets/images/54-485x360.jpg.webp";
 import LinkArrows from "../../Components/LinkArrows/LinkArrows";
 
+const section_1 = [
+	{
+		image: image_105,
+		content: "Have a look around this bold and colourful 1930s semi in London",
+	},
+	{
+		image: image_104,
+		content: "Outdoor Kitchen Decorating: Furnish and Accessorize with Flair",
+	},
+	{
+		image: image_101,
+		content: "Interior Design Tips: Decorating to Celebrate the Great Outdoors",
+	},
+	{
+		image: image_91,
+		content: "Citroen Could Revive the Third Car for Loeb/Breen Winning Couple",
+	},
+];
+
 const section = [
 	{
 		image: image_max,
@@ -80,7 +99,7 @@ function News() {
 								<div className='flex flex-col md:grid md:grid-cols-2 md:gap-[4%] w-full gap-4'>
 									{section.map(
 										(section, index) =>
-											index < 4 && (
+											index < 6 && (
 												<>
 													<div className='flex w-full'>
 														<div className='w-full'>
@@ -107,7 +126,35 @@ function News() {
 					{/* End section */}
 				</div>
 				<div className='w-full md:w-[31%]'>
-					<div className='w-full'>{/* <SideNav /> */}</div>
+					<div className='w-full flex flex-col gap-5 sticky top-5'>
+						<Heading
+							border_color={"border-b-[2px] border-b-[#222222]"}
+							bg_color={"bg-[#222222] text-white"}
+							text_color={"text-[#3a863d]"}
+							heading_text={"MOST POPULAR"}
+							links={false}
+						/>
+						<div className='flex w-full'>
+							<div className='w-full flex flex-col gap-4'>
+								{section_1.map((section, index) => (
+									<>
+										<Card
+											key={index}
+											card_class={"flex gap-5 w-full"}
+											image_class={
+												"w-[100px] h-[90px] sm:w-[200px] sm:h-[140px] md:w-[100px] h-[60px] md:h-[80px]"
+											}
+											image={section.image}
+											title={section.content}
+											author={"author"}
+											text_hover={" text-[#f9c100] "}
+											text_size={"text-sm"}
+										/>
+									</>
+								))}
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
